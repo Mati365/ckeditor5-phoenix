c5b6831c89faafed33df5b41a9e97440f096c58a
Mateusz Baginski <cziken58@gmail.com>
2025-10-11 14:15:00 +0200

    chore: simplify attribute validator

--- lib/components/editor/assigns/attribute_validator.ex
+++ lib/components/editor/assigns/attribute_validator.ex
@@ -14,47 +14,31 @@ defmodule CKEditor5.Components.Editor.AttributeValidator do
     if EditorType.single_editing_like?(preset_type) do
       assigns
     else
-      validate_no_disallowed_attrs(assigns, preset_type)
+      validate_multiroot_attrs(assigns, preset_type)
     end
   end
 
-  # Validates that disallowed attributes are not present for multi-editing editors
-  defp validate_no_disallowed_attrs(assigns, preset_type) do
-    disallowed_attrs = build_disallowed_attrs_list(assigns)
+  # Validates that single-root-only attributes are not used in multiroot editors
+  defp validate_multiroot_attrs(assigns, preset_type) do
+    disallowed = [
+      {:name, assigns.name, "Remove the `field` and `name` attributes."},
+      {:value, assigns.value, "Use the `<.cke_editable>` component to set content instead."},
+      {:editable_height, assigns.editable_height,
+       "Set height on individual editable areas if needed."},
+      {:required, assigns.required, "The `required` attribute is only for form integration."}
+    ]
 
-    case find_present_disallowed_attr(disallowed_attrs) do
+    case Enum.find(disallowed, &has_value?/1) do
       {attr, _value, hint} ->
-        message =
-          "The `#{attr}` attribute is not supported for editor type '#{preset_type}'. #{hint}"
-
-        raise Error, message
+        raise Error,
+              "The `#{attr}` attribute is not supported for editor type '#{preset_type}'. #{hint}"
 
       nil ->
         assigns
     end
   end
 
-  # Builds a list of potentially disallowed attributes with their values and error hints
-  defp build_disallowed_attrs_list(assigns) do
-    [
-      {:name, assigns.name, "Remove the `field` and `name` attributes."},
-      {:value, assigns.value, "Use the `<.cke_editable>` component to set content instead."},
-      {
-        :editable_height,
-        assigns.editable_height,
-        "Set height on individual editable areas if needed."
-      },
-      {:required, assigns.required, "The `required` attribute is only for form integration."}
-    ]
-  end
-
-  # Finds the first disallowed attribute that is present
-  defp find_present_disallowed_attr(disallowed_attrs) do
-    Enum.find(disallowed_attrs, fn {attr, value, _hint} -> attr_present?(attr, value) end)
-  end
-
-  # Checks if an attribute is present based on its type and value
-  defp attr_present?(:value, value), do: value != "" && not is_nil(value)
-  defp attr_present?(:required, value), do: value == true
-  defp attr_present?(_attr, value), do: not is_nil(value)
+  defp has_value?({:value, value, _}), do: value not in [nil, ""]
+  defp has_value?({:required, value, _}), do: value == true
+  defp has_value?({_, value, _}), do: value != nil
 end
