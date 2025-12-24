d3ee528e681fc132fb9f2673260ba9456e10c737
Mateusz Baginski <cziken58@gmail.com>
2025-12-24 11:38:17 +0100

    feat: bump default CKEditor cloud version to `47.3.0`

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule CKEditor5.MixProject do
 
   @version "1.15.1"
   @source_url "https://github.com/Mati365/ckeditor5-phoenix"
-  @default_cke_version "47.2.0"
+  @default_cke_version "47.3.0"
 
   def project do
     [
