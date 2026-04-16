8a7aacc9713126f9e6b1dfd85620e2c6eca11ca0
Mateusz Baginski <cziken58@gmail.com>
2026-04-16 08:03:38 +0200

    test: adjust e2e tests

--- test/e2e/context_test.exs
+++ test/e2e/context_test.exs
@@ -5,8 +5,8 @@ defmodule Playground.ContextTest do
     session
     |> visit(~p"/context")
     |> assert_has(css(".ck-editor__editable", count: 2))
-    |> type_in_editor("#editor-a .ck-editor__editable", " in A")
-    |> type_in_editor("#editor-b .ck-editor__editable", " in B")
+    |> type_in_editor("#container-first .ck-editor__editable", " in A")
+    |> type_in_editor("#container-second .ck-editor__editable", " in B")
     |> assert_text(" in A")
     |> assert_text(" in B")
   end
